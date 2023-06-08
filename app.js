4787d0ca51bdddbc017bf13a589ed0a8935ba261
JulianRuiz96 <davidjul97@gmail.com>
2023-06-07 23:52:12 -0500

    Lista contacto 2

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 const listaContactos = [
-  "Julian Ruiz",
-  "Diana Morales",
-  "Nicolas Garcia",
-  "Laura Rodriguez",
+  { id: 1, nombres: "Julian", apellidos: "Ruiz" },
+  { id: 2, nombres: "Diana", apellidos: "Morales" },
+  { id: 3, nombres: "Nicolas", apellidos: "Garcia" },
+  { id: 4, nombres: "Laura", apellidos: "Rodriguez" },
 ];
 
 console.log(listaContactos);
 
-function agregarContacto(nombreCompleto) {
-  listaContactos.push(nombreCompleto);
+function agregarContacto(id, nombres, apellidos) {
+  const contacto = { id, nombres, apellidos };
+  listaContactos.push(contacto);
 }
 
-agregarContacto("Julian Morales");
+agregarContacto(5, "Julian", "Morales");
 
 console.log(listaContactos);
 
-function borrarContacto(nombreCompleto) {
-  const indice = listaContactos.indexOf(nombreCompleto);
+function borrarContacto(id) {
+  const indice = listaContactos.findIndex((contacto) => contacto.id === id);
   if (indice !== -1) {
+    const nombreCompleto = `${listaContactos[indice].nombres} ${listaContactos[indice].apellidos}`;
     listaContactos.splice(indice, 1);
     console.log(`Contacto ${nombreCompleto} borrado correctamente.`);
   } else {
-    console.log(`El contacto ${nombreCompleto} no existe en la lista.`);
+    console.log(`El contacto con ID ${id} no existe en la lista.`);
   }
 }
 
-
-borrarContacto("Julian Morales");
-
+borrarContacto(5);
 
 function imprimirContactos() {
   console.log("Contactos en la lista:");
   for (let i = 0; i < listaContactos.length; i++) {
-    console.log(listaContactos[i]);
+    const { id, nombres, apellidos } = listaContactos[i];
+    console.log(`ID: ${id}, Nombres: ${nombres}, Apellidos: ${apellidos}`);
   }
 }
 
 imprimirContactos();
-
-
